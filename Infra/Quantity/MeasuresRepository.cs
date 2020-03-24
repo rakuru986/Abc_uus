9e81d65a839b43db97e4588d0d3e1b5edc342e15
Rasmus Kurušev <rakuru@ttu.ee>
2020-03-24 10:24:39 +0200

    Revert "42/44"
    This reverts commit d9c484ed9c2fccca1a84939697aff47cc0958a9b.

---
+++ Infra/Quantity/MeasuresRepository.cs
@@ -0,0 +1,13 @@
+﻿using Abc.Data.Quantity;
+using Abc.Domain.Quantity;
+
+namespace Abc.Infra.Quantity
+{
+    public class MeasuresRepository : UniqueEntityRepository<Measure, MeasureData>, IMeasuresRepository
+    {
+        public MeasuresRepository(QuantityDbContext c) : base(c, c.Measures) { }
+
+        protected internal override Measure toDomainObject(MeasureData d) => new Measure(d);
+
+    }
+}
