2ed08804c20215b5b3eb2bfda351df29a0534ca2
Rasmus Kurušev <rakuru@ttu.ee>
2020-03-31 12:28:53 +0300

    45/52

---
+++ Data/Quantity/CommonTerm.cs
@@ -0,0 +1,11 @@
+﻿using Abc.Data.Common;
+
+namespace Abc.Data.Quantity
+{
+    public class CommonTerm : PeriodData
+    {
+        public string MasterId { get; set; }
+        public string TermId { get; set; }
+        public int Power { get; set; }
+    }
+}
---
+++ Data/Quantity/MeasureTerm.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abc.Data.Quantity
+{
+    public class MeasureTerm : CommonTerm
+    {
+    }
+}
